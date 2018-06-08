25965c18047d850c55254537d173f13173552cd0
Roberto aguero <rjaguero00@gmail.com>
2018-06-08 16:06:36 -0700

    nav and jumbo/still need editing

---
+++ src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Jumbotron.css";
+
+const Jumbotron = () => (
+    <div className="container">
+        <div className="jumbotron jumbotron-fluid">
+            <div className="container">
+                <h1 className="display-4">Westworld Click Game</h1>
+                <p className="lead">Click on an image to earn points, but don't click on any more than once!.</p>
+            </div>
+        </div>
+    </div>
+);
+
+export default Jumbotron;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => (
+    <div className="container">
+        <nav className="navbar navbar-light bg-light">
+            <span className="navbar-brand mb-0 h1">WestWrold</span>
+            <span className="navbar-brand mb-0 h2">Click an image to begin!</span>
+            <span className="navbar-brand mb-0 h3">Score: 0 | Top Score: 0</span>
+        </nav>
+    </div>
+);
+
+export default Navbar;
