c272c26250286145434b9eabb305809cc9fba1e4
Roberto aguero <rjaguero00@gmail.com>
2018-06-08 22:08:34 -0700

    navbar/jumbo/vikingcards still need to render cards

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -2,10 +2,10 @@ import React from "react";
 import "./Jumbotron.css";
 
 const Jumbotron = () => (
-    <div className="container">
+    <div className="jumbotron-container">
         <div className="jumbotron jumbotron-fluid">
             <div className="container">
-                <h1 className="display-4">Westworld Click Game</h1>
+                <h1 className="display-4">Vikings Click Game</h1>
                 <p className="lead">Click on an image to earn points, but don't click on any more than once!.</p>
             </div>
         </div>
