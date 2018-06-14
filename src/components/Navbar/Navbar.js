141ec4361c39fbc14b0c36297274fd7b7ef0c3ac
Roberto aguero <rjaguero00@gmail.com>
2018-06-13 20:36:05 -0700

    navbar font color

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import "./Navbar.css";
 
 const Navbar = (props) => (
     <div className="nav-container">
-        <nav className="navbar navbar-light">
+        <nav className="navbar navbar">
             <span className="navbar-brand mb-0 h1">Vikings</span>
             <span className="navbar-brand mb-0 h2">{props.message}</span>
             <span className="navbar-brand mb-0 h3">Score: {props.score}</span>
